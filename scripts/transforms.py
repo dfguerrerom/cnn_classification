50b70b2e27aba55dcd6b1a3d2cf3b1f37b64dc1e
dfguerrerom <dfgm2006@gmail.com>
2022-07-18 17:32:02 +0000

    inherit transforms to read the specific structure from the dataset

---
+++ scripts/transforms.py
@@ -0,0 +1,35 @@
+from torchvision import transforms, utils
+from torchvision.transforms import Resize, Normalize
+
+class rescale:
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        
+        image, label = sample['image'], sample['label']
+        return {'image': Resize(self.output_size)(image), 'label': label}
+    
+class normalize:
+    
+    def __init__(self, mean, std):
+        
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, sample):
+        
+        image, label = sample['image'], sample['label']
+        return {
+            'image': Normalize(mean=self.mean, std=self.std)(image), 
+            'label': label
+        }

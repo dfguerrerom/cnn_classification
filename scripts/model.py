474a2ea7a70ddabe4db1b96e03f84ecb99f93c4c
dfguerrerom <dfgm2006@gmail.com>
2022-07-21 08:43:43 +0000

    refactor: allow use fine tune or fixed features for pretrained models, so we can play easier

--- scripts/model.py
+++ scripts/model.py
@@ -1,17 +1,32 @@
-
+import torch
 import torch.nn as nn
 from torch.optim import Adam, lr_scheduler
 from torchvision import models
 
-model = models.resnet18(pretrained=True)
-for param in model.parameters():
-    param.require_grad = False
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def get_model(output_size, fixed_feature=False,):
+    """get corresponding pretrained model. Either fixed_feature or fine_tune"""
     
-num_ftrs = model.fc.in_features
-# Here the size of each output sample is set to 10.
-model.fc = nn.Linear(num_ftrs, 19)
- 
-# Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
+    model = models.resnet34(pretrained=True)
+    num_ftrs = model.fc.in_features
+
+    if fixed_feature:
+        # ConvNet as fixed feature extractor
+        _=[setattr(param, "require_grad", False) for param in model.parameters()]
+    
+    model.fc = nn.Linear(num_ftrs, output_size)
+    
+    return model
+
+
+model = get_model(6, fixed_feature=False)
+model.to(device)
+
+
+# Define the loss function with Classification Cross-Entropy loss and an optimizer
+# with Adam optimizer
+
 optimizers = {
     "Adam" : Adam(model.parameters(), lr=0.1, weight_decay=0.0001)
 }
@@ -29,6 +44,5 @@ loss_fn = loss_fns[loss_fn_name]
 
 lambda1 = lambda epoch: 0.65 ** epoch
 scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda1)
-
 
 

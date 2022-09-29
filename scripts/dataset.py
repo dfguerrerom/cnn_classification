b19725081f0a14be429a5d7e898724908cbd549e
dfguerrerom <dfgm2006@gmail.com>
2022-09-29 19:04:47 +0000

    style(feat): pass style formatters and function to retrieve prediction settings

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,8 +1,10 @@
 import os
 from typing import Dict, Tuple
 
-if "GDAL_DATA" in list(os.environ.keys()): del os.environ["GDAL_DATA"]
-if "PROJ_LIB" in list(os.environ.keys()): del os.environ["PROJ_LIB"]
+if "GDAL_DATA" in list(os.environ.keys()):
+    del os.environ["GDAL_DATA"]
+if "PROJ_LIB" in list(os.environ.keys()):
+    del os.environ["PROJ_LIB"]
 
 from collections import Counter
 from pathlib import Path
@@ -10,12 +12,10 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import rasterio as rio
 import torch
 import torch.nn.functional as F
 from matplotlib import pyplot as plt
-from plotly.subplots import make_subplots
 from torchgeo.datasets import VisionDataset
 
 from . import scripts
@@ -24,28 +24,28 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 class PlanetDataSet(VisionDataset):
-    
-    def __init__(self, 
-                 root, 
-                 data_df, 
-                 label_col, 
-                 ext=".tif", 
-                 transforms=None, 
-                 multilabel=False,
-                 fixed_tags=None,
-                 sep=","
-        ):
+    def __init__(
+        self,
+        root,
+        data_df,
+        label_col,
+        ext=".tif",
+        transforms=None,
+        multilabel=False,
+        fixed_tags=None,
+        sep=",",
+    ):
         """Initialize a new VisionDataset instance.
         Args:
             root (str): path where the images are located
-            data_df (pd.DataFrame): pandas dataframe containing id with image names and 
+            data_df (pd.DataFrame): pandas dataframe containing id with image names and
                 labels cols
             label_col (str): name of the column to use as labels column
             transforms: a function/transform that takes input sample and its target as
                 entry and returns a transformed version
             fixed_tags (dict(name:number))
         """
-        
+
         self.transforms = transforms
         self.root = root
         self.data_df = data_df.reset_index(drop=True)
@@ -53,12 +53,12 @@ class PlanetDataSet(VisionDataset):
         self.ext = ext
         self.multilabel = multilabel
         self.sep = sep
-        
+
         self.classes, self.class_to_idx = self.get_tags()
-        
+
         # Override if we are using some fixed tags
         self.class_to_idx = fixed_tags or self.class_to_idx
-        
+
         self.classes = self.classes["tag"].values
 
     def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
@@ -69,23 +69,25 @@ class PlanetDataSet(VisionDataset):
             data and label at that index
         """
         image = self._load_image(index)
-                        
+
         if self.multilabel:
-            
-            label = torch.LongTensor([
-                 self.class_to_idx[lbl] for lbl in
-                 self.data_df.loc[index][self.label_col].split(self.sep)  
-             ])
+
+            label = torch.LongTensor(
+                [
+                    self.class_to_idx[lbl]
+                    for lbl in self.data_df.loc[index][self.label_col].split(self.sep)
+                ]
+            )
             label = F.one_hot(label, num_classes=len(self.class_to_idx))
             label = label.sum(dim=0)
-            
+
         else:
             label = torch.tensor(
                 [self.class_to_idx[self.data_df.loc[index][self.label_col]]]
             ).type(torch.LongTensor)
 
         sample = {"image": image.float(), "label": label.to(device)}
-        
+
         if self.transforms:
             sample = self.transforms(sample)
 
@@ -97,7 +99,7 @@ class PlanetDataSet(VisionDataset):
             length of the dataset
         """
         return len(self.data_df)
-    
+
     def _load_image(self, index: int) -> Tuple[torch.Tensor]:
         """Load a single image and it's class label.
         Args:
@@ -106,56 +108,59 @@ class PlanetDataSet(VisionDataset):
             the image
             the image class label
         """
-        
+
         image_id = self.data_df.loc[index]["id"]
-        image_path = (self.root/image_id).with_suffix(self.ext)
-        
+        image_path = (self.root / image_id).with_suffix(self.ext)
+
         with rio.open(image_path, "r") as src:
             array = src.read()
-            tensor = torch.from_numpy(array[[2,1,0],...]).to(device)
+            tensor = torch.from_numpy(array[[2, 1, 0], ...]).to(device)
             tensor = scripts.normalize_mean_std(tensor)
             return tensor
-    
-    def plot(self, index, rgb=[2,1,0], display=True):
+
+    def plot(self, index, rgb=[2, 1, 0], display=True):
         """Get image from index and plot it using rgb combination"""
-        
+
         sample = self.__getitem__(index)
-        
+
         # its shape is: Ch.H.W.
         normalized_img = scripts.normalize_tensor(sample["image"])
         # Permute them to be displayed by matplot
-        normalized_img = normalized_img.permute(1,2,0)
+        normalized_img = normalized_img.permute(1, 2, 0)
         # Combine bands in the given rgb order
         normalized_img = normalized_img[..., rgb].cpu().detach()
 
-        if not display: return normalized_img
-        
+        if not display:
+            return normalized_img
+
         image_name = self.data_df.loc[index].id
         tag = self.data_df.loc[index][self.label_col]
-        
+
         fig, axes = plt.subplots(1, 1, figsize=(5, 5))
-        
+
         axes.imshow(normalized_img)
         axes.set_title(f"{tag}\n{image_name}")
-        
+
         return fig.show()
-    
+
     def plot_tags(self, size=20):
         """Plot all tags and images within the dataset"""
-        
+
         tags, _ = self.get_tags()
         tags = tags["tag"].unique()
-        
+
         n_cols = min(len(tags), 4)
-        
-        fig, axes = plt.subplots(len(tags)//n_cols+1, n_cols, figsize=(size, size))
-        
+
+        fig, axes = plt.subplots(len(tags) // n_cols + 1, n_cols, figsize=(size, size))
+
         for idx, tag in enumerate(tags):
-            
-            image_id = self.data_df[
-                self.data_df[self.label_col].isin(tags)
-            ].sample().index.values[0]
-            
+
+            image_id = (
+                self.data_df[self.data_df[self.label_col].isin(tags)]
+                .sample()
+                .index.values[0]
+            )
+
             image_name = self.data_df.loc[image_id].id
 
             col = idx % n_cols
@@ -163,52 +168,50 @@ class PlanetDataSet(VisionDataset):
             if isinstance(axes, np.ndarray):
                 if isinstance(axes[row], np.ndarray):
                     axes[row][col].set_title(f"{tag}\n{image_name}")
-                    axes[row][col].imshow(self.plot(image_id, display=False));
+                    axes[row][col].imshow(self.plot(image_id, display=False))
                 else:
                     axes[col].set_title(f"{tag}\n{image_name}")
-                    axes[col].imshow(self.plot(image_id, display=False));
+                    axes[col].imshow(self.plot(image_id, display=False))
             else:
                 axes.set_title(f"{tag}\n{image_name}")
-                axes.imshow(self.plot(image_id, display=False));
+                axes.imshow(self.plot(image_id, display=False))
 
         return fig
-        
-    def get_histogram(self,):
-        """"Plot all the classes present in the dataset on an histogram"""
-        
+
+    def get_histogram(self):
+        """ "Plot all the classes present in the dataset on an histogram"""
+
         df_tags, _ = self.get_tags()
         fig = px.bar(
-            df_tags, 
-            x="total", 
-            y="tag", 
-            orientation="h", 
+            df_tags,
+            x="total",
+            y="tag",
+            orientation="h",
             color="total",
         )
-        
+
         fig.update_layout(title=f"{self.label_col} distribution")
-        
+
         return fig.show()
-        
+
     def get_tags(self):
         """Get the tags frequency from the original dataset"""
-        
+
         df = self.data_df[self.label_col].copy()
         df = df.astype(str)
 
-        tags = (df
-            if not self.multilabel else [
-                tag
-                for row in df.values
-                for tag in row.split(self.sep)
-            ]
+        tags = (
+            df
+            if not self.multilabel
+            else [tag for row in df.values for tag in row.split(self.sep)]
         )
-        
+
         tags_count = pd.DataFrame(
             list(Counter(tags).items()), columns=["tag", "total"]
         ).sort_values(by="total")
-        
+
         class_to_idx = {
-            v:k for k,v in tags_count.reset_index().to_dict()["tag"].items()
+            v: k for k, v in tags_count.reset_index().to_dict()["tag"].items()
         }
-        
+
         return tags_count, class_to_idx
--- scripts/model.py
+++ scripts/model.py
@@ -1,129 +1,169 @@
+from importlib import reload
+
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.optim import Adam, lr_scheduler
 from torchvision import models
-import torch.optim as optim
-
-import config.settings as ss
-
-from importlib import reload
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def get_model(model_name, output_size, fixed_feature=False,):
+
+def get_model(model_name, output_size, fixed_feature=False):
     """get corresponding pretrained model. Either fixed_feature or fine_tune
-    
-    output_size: is the number of the output classes
+
+    output_size: is the number of the output classes, it has to match
+
+
     """
-    
+
     if model_name in ["dpn92", "dpn131"]:
         model = torch.hub.load(
-            'rwightman/pytorch-dpn-pretrained', model_name, pretrained=True
+            "rwightman/pytorch-dpn-pretrained", model_name, pretrained=True
         )
-    
+
     elif model_name in ["resnet34"]:
         model = models.resnet34(pretrained=True)
-    
+
     # when the model is dpn, it will return a tuple
     model = next(iter(model)) if isinstance(model, (tuple, list)) else model
-    
+
     # Get the last layer name
     fc_layer_name = list(model.named_modules())[-1][0]
     fc_layer = getattr(model, fc_layer_name)
-    
+
     # Get the input number of features
-    # in_features when using resnet, in_cahnels when using dpn 
-    num_ftrs = getattr(fc_layer, "in_features", None) or getattr(fc_layer, "in_channels")
+    # in_features when using resnet, in_cahnels when using dpn
+    num_ftrs = getattr(fc_layer, "in_features", None) or getattr(
+        fc_layer, "in_channels"
+    )
 
     if fixed_feature:
         # fixed feature extractor
-        _=[setattr(param, "require_grad", False) for param in model.parameters()]
-    
+        _ = [setattr(param, "require_grad", False) for param in model.parameters()]
+
     if "resnet" in model_name:
         last_layer = nn.Linear(num_ftrs, output_size)
     else:
         last_layer = nn.Conv2d(num_ftrs, output_size, kernel_size=1, bias=True)
-    
+
     # Let's add the last layer to the model
     setattr(model, fc_layer_name, last_layer)
-    
+
     return model
 
 
-def get_optimizer(name, model, **kwargs ):
+def get_optimizer(name, model, **kwargs):
     """Returns the optimizer based on the input name and specific args"""
-    
+
     if name == "Adam":
-        
+
         lr = kwargs.get("lr")
         weight_decay = kwargs.get("weight_decay")
         return Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-    
+
     elif name == "SDG":
-        
+
         lr = kwargs.get("lr")
         momentum = kwargs.get("momentum")
         return optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
+
 def get_scheduler(name, optimizer, **kwargs):
     """Returns the scheduler based on the input name and specific args"""
-    
+
     if name == "LambdaLR":
-        
-        lr_lambda= kwargs.get("lr_lambda")
+
+        lr_lambda = kwargs.get("lr_lambda")
         return lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
-    
+
     elif name == "StepLR":
-        
-        step_size= kwargs.get("step_size")
-        gamma= kwargs.get("gamma")
+
+        step_size = kwargs.get("step_size")
+        gamma = kwargs.get("gamma")
         return lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
 
+
 loss_fns = {
-    "CrossEntropy" : nn.CrossEntropyLoss(),
-    "MSELoss" : nn.MSELoss(),
+    "CrossEntropy": nn.CrossEntropyLoss(),
+    "MSELoss": nn.MSELoss(),
 }
+
 
 def get_settings(test_name):
     """Return the variables to be used in the training step based on some
     predefined model settings"""
-    
+
     import config.settings as ss
+
     ss = reload(ss)
     settings = ss.settings
-    
+
     setting = settings[test_name]
     model_name = setting.model.name
-    
+
     model = get_model(
-        model_name = model_name,
-        output_size = setting.model.out_features,
-        fixed_feature = setting.model.fixed_feature
+        model_name=model_name,
+        output_size=setting.model.out_features,
+        fixed_feature=setting.model.fixed_feature,
     )
-    
+
     model.to(device)
-    
+
     optimizer = get_optimizer(
-        name = setting.optimizer.name,
-        model = model,
-        lr = setting.optimizer.lr,
-        weight_decay = getattr(setting.optimizer, "weight_decay", None),
-        momentum = getattr(setting.optimizer, "momentum", None),
+        name=setting.optimizer.name,
+        model=model,
+        lr=setting.optimizer.lr,
+        weight_decay=getattr(setting.optimizer, "weight_decay", None),
+        momentum=getattr(setting.optimizer, "momentum", None),
     )
-    
+
     scheduler = get_scheduler(
-        setting.scheduler.name, 
-        optimizer, 
-        step_size = setting.scheduler.get("step_size"),
-        gamma = setting.scheduler.get("gamma"),
-        lr_lambda = setting.scheduler.get("lr_lambda"),
+        setting.scheduler.name,
+        optimizer,
+        step_size=setting.scheduler.get("step_size"),
+        gamma=setting.scheduler.get("gamma"),
+        lr_lambda=setting.scheduler.get("lr_lambda"),
     )
-    
+
     loss_fn = loss_fns[setting.loss_fn.name]
     variable = setting.dataset.variable
     batch_size = setting.batch_size
     rescale_factor = setting.rescale_factor
-    
-    metadata = setting.to_json(default=lambda o: '<not serializable>')
-    
-    return model, model_name, optimizer, loss_fn, scheduler, variable, batch_size, rescale_factor, metadata
+
+    metadata = setting.to_json(default=lambda o: "<not serializable>")
+
+    return (
+        model,
+        model_name,
+        optimizer,
+        loss_fn,
+        scheduler,
+        variable,
+        batch_size,
+        rescale_factor,
+        metadata,
+    )
+
+
+def get_prediction_settings(test_name):
+    """
+    Will return the settings variables that corresponds to test_name and return:
+    stride, width, height, batch_size
+    """
+
+    import config.settings as ss
+
+    ss = reload(ss)
+    settings = ss.settings
+
+    setting = settings[test_name]
+
+    stride = setting.prediction.stride
+    width = setting.prediction.width
+    height = setting.prediction.height
+    batch_size = setting.prediction.batch_size
+
+    metadata = setting.prediction.to_json(default=lambda o: "<not serializable>")
+
+    return stride, width, height, batch_size, metadata
